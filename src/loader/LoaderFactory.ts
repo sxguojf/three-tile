4fe68d6c2c2747ff2fa33b1bcd086dda6de3f8e3
hz_gjf@163.com <gjf123456>
2024-06-23 01:04:00 +0800

    LoaderFactory由类改为简单对象

--- src/loader/LoaderFactory.ts
+++ src/loader/LoaderFactory.ts
@@ -11,56 +11,56 @@ import { ITileGeometryLoader, ITileMaterialLoader } from "./ITileLoaders";
 /**
  * factory for loader
  */
-export class LoaderFactory {
-	public static manager = new LoadingManager();
+export const LoaderFactory = {
+	manager: new LoadingManager(),
 	// dict of dem loader
-	private static demLoaderMap = new Map<string, ITileGeometryLoader>();
+	demLoaderMap: new Map<string, ITileGeometryLoader>(),
 	// dict of img loader
-	private static imgLoaderMap = new Map<string, ITileMaterialLoader>();
+	imgLoaderMap: new Map<string, ITileMaterialLoader>(),
 
 	/**
 	 * register material loader
 	 * @param loader material loader
 	 */
-	public static registerMaterialLoader(loader: ITileMaterialLoader) {
+	registerMaterialLoader(loader: ITileMaterialLoader) {
 		LoaderFactory.imgLoaderMap.set(loader.dataType, loader);
 		console.log(`* Register imageLoader: ${loader.dataType}`);
-	}
+	},
 
 	/**
 	 * register geometry loader
 	 * @param loader geometry loader
 	 */
-	public static registerGeometryLoader(loader: ITileGeometryLoader) {
+	registerGeometryLoader(loader: ITileGeometryLoader) {
 		LoaderFactory.demLoaderMap.set(loader.dataType, loader);
 		console.log(`* Register terrainLoader: ${loader.dataType}`);
-	}
+	},
 
 	/**
 	 * get material loader from datasource
 	 * @param source datasource
 	 * @returns material loader
 	 */
-	public static getMaterialLoader(source: ISource) {
+	getMaterialLoader(source: ISource) {
 		const loader = LoaderFactory.imgLoaderMap.get(source.dataType);
 		if (loader) {
 			return loader;
 		} else {
 			throw `Source dataType "${source.dataType}" is not support!`;
 		}
-	}
+	},
 
 	/**
 	 * get geometry loader from datasource
 	 * @param source datasouce
 	 * @returns geometry loader
 	 */
-	public static getGeometryLoader(source: ISource) {
+	getGeometryLoader(source: ISource) {
 		const loader = LoaderFactory.demLoaderMap.get(source.dataType);
 		if (loader) {
 			return loader;
 		} else {
 			throw `Source dataType "${source.dataType}" is not support!`;
 		}
-	}
-}
+	},
+};
