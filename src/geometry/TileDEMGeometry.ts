bfad76fad19971a0657486d3daa062b825094591
hz_gjf@163.com <gjf123456>
2024-12-14 12:27:39 +0800

    重写法向量计算

--- src/geometry/TileDEMGeometry.ts
+++ src/geometry/TileDEMGeometry.ts
@@ -130,36 +130,91 @@ export class TileDEMGeometry extends PlaneGeometry {
 	// set normal on edge(skrit)
 	// 瓦片边缘法向量计算比较复杂，需要根据相邻瓦片高程计算，暂未完美实现
 	// 考虑使用Mapbox Terrain-DEM v1格式地形 https://docs.mapbox.com/data/tilesets/reference/mapbox-terrain-dem-v1/
-	public computeVertexNormals() {
-		super.computeVertexNormals();
 
+	computeVertexNormals() {
 		const index = this.index;
 		const positionAttribute = this.getAttribute("position");
-		const normalAttribute = this.getAttribute("normal");
 
-		const pA = new Vector3(),
-			pB = new Vector3(),
-			pC = new Vector3();
+		if (positionAttribute !== undefined) {
+			let normalAttribute = this.getAttribute("normal");
 
-		if (index) {
-			for (let i = 0; i < index.count; i += 3) {
-				// 取得一个三角形的三个顶点索引
-				const vA = index.getX(i + 0);
-				const vB = index.getX(i + 1);
-				const vC = index.getX(i + 2);
+			if (normalAttribute === undefined) {
+				normalAttribute = new BufferAttribute(new Float32Array(positionAttribute.count * 3), 3);
+				this.setAttribute("normal", normalAttribute);
+			} else {
+				// reset existing normals to zero
 
-				// 取出三个点的position
-				pA.fromBufferAttribute(positionAttribute, vA);
-				pB.fromBufferAttribute(positionAttribute, vB);
-				pC.fromBufferAttribute(positionAttribute, vC);
-
-				// 三个点中有一个点在边缘，重置法向量
-				if (pA.z < this._min || pB.z < this._min || pC.z < this._min) {
-					normalAttribute.setXYZ(vA, 0, 0, 1);
-					normalAttribute.setXYZ(vB, 0, 0, 1);
-					normalAttribute.setXYZ(vC, 0, 0, 1);
+				for (let i = 0, il = normalAttribute.count; i < il; i++) {
+					normalAttribute.setXYZ(i, 0, 0, 0);
 				}
 			}
+
+			const pA = new Vector3(),
+				pB = new Vector3(),
+				pC = new Vector3();
+			const nA = new Vector3(),
+				nB = new Vector3(),
+				nC = new Vector3();
+			const cb = new Vector3(),
+				ab = new Vector3();
+
+			// indexed elements
+
+			if (index) {
+				for (let i = 0, il = index.count; i < il; i += 3) {
+					const vA = index.getX(i + 0);
+					const vB = index.getX(i + 1);
+					const vC = index.getX(i + 2);
+
+					pA.fromBufferAttribute(positionAttribute, vA);
+					pB.fromBufferAttribute(positionAttribute, vB);
+					pC.fromBufferAttribute(positionAttribute, vC);
+
+					// 三个点中有一个点在边缘，重置法向量
+					if (pA.z < this._min || pB.z < this._min || pC.z < this._min) {
+						normalAttribute.setXYZ(vA, 0, 0, 1);
+						normalAttribute.setXYZ(vB, 0, 0, 1);
+						normalAttribute.setXYZ(vC, 0, 0, 1);
+					}
+					{
+						cb.subVectors(pC, pB);
+						ab.subVectors(pA, pB);
+						cb.cross(ab);
+
+						nA.fromBufferAttribute(normalAttribute, vA);
+						nB.fromBufferAttribute(normalAttribute, vB);
+						nC.fromBufferAttribute(normalAttribute, vC);
+
+						nA.add(cb);
+						nB.add(cb);
+						nC.add(cb);
+
+						normalAttribute.setXYZ(vA, nA.x, nA.y, nA.z);
+						normalAttribute.setXYZ(vB, nB.x, nB.y, nB.z);
+						normalAttribute.setXYZ(vC, nC.x, nC.y, nC.z);
+					}
+				}
+			} else {
+				// non-indexed elements (unconnected triangle soup)
+
+				for (let i = 0, il = positionAttribute.count; i < il; i += 3) {
+					pA.fromBufferAttribute(positionAttribute, i + 0);
+					pB.fromBufferAttribute(positionAttribute, i + 1);
+					pC.fromBufferAttribute(positionAttribute, i + 2);
+
+					cb.subVectors(pC, pB);
+					ab.subVectors(pA, pB);
+					cb.cross(ab);
+
+					normalAttribute.setXYZ(i + 0, cb.x, cb.y, cb.z);
+					normalAttribute.setXYZ(i + 1, cb.x, cb.y, cb.z);
+					normalAttribute.setXYZ(i + 2, cb.x, cb.y, cb.z);
+				}
+			}
+
+			this.normalizeNormals();
+
+			normalAttribute.needsUpdate = true;
 		}
 	}
 }
