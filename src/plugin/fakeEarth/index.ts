d50534b0265b9a2ca854fa65670c1084ff86b849
hz_gjf@163.com <gjf123456>
2025-03-04 23:50:54 +0800

    -

--- src/plugin/fakeEarth/index.ts
+++ src/plugin/fakeEarth/index.ts
@@ -5,10 +5,9 @@
  */
 
 import { Color, Scene } from "three";
-import { TileMap } from "../../map";
-import { BasePlugin } from "../PluginSDK";
-import { FakeEarth } from "./FakeEarth";
 import { MapControls } from "three/examples/jsm/controls/MapControls";
+import { TileMap } from "../../map";
+import { FakeEarth } from "./FakeEarth";
 
 export { EarthMaskMaterial } from "./EarthMaskMaterial";
 export { FakeEarth } from "./FakeEarth";
@@ -19,34 +18,33 @@ export type FrakeEarthOptions = {
 	controls: MapControls;
 };
 
-class FrakeEarthPlugin extends BasePlugin {
-	protected doInstall(options: FrakeEarthOptions): void {
-		const { map, scene, controls } = options;
-		const fakeEarth = new FakeEarth(scene.fog?.color || new Color(0));
-		fakeEarth.name = "fakeearth";
-		fakeEarth.applyMatrix4(map.rootTile.matrix);
-		map.add(fakeEarth);
+// class FrakeEarthPlugin extends BasePlugin {
+// 	protected doInstall(options: FrakeEarthOptions): void {
+// 		const { map, scene, controls } = options;
+// 		const fakeEarth = new FakeEarth(scene.fog?.color || new Color(0));
+// 		fakeEarth.name = "fakeearth";
+// 		fakeEarth.applyMatrix4(map.rootTile.matrix);
+// 		map.add(fakeEarth);
 
-		controls.addEventListener("change", () => {
-			// 地图距摄像机较远时再显示遮罩
-			fakeEarth.visible = controls.getDistance() > 3000;
-		});
-	}
-}
+// 		controls.addEventListener("change", () => {
+// 			// 地图距摄像机较远时再显示遮罩
+// 			fakeEarth.visible = controls.getDistance() > 3000;
+// 		});
+// 	}
+// }
 
-export const frakEarth = new FrakeEarthPlugin();
+// export const frakEarth = new FrakeEarthPlugin();
 
 declare module "../../map" {
 	interface TileMap {
-		addFrakEarth(scene: Scene): this;
+		addFrakEarth(scene: Scene): FakeEarth;
 	}
 }
-
-TileMap.prototype.addFrakEarth = function (scene: Scene): TileMap {
-	console.log("this is a test");
+// 扩展TileMap类，原型链上添加addFrakEarth方法
+TileMap.prototype.addFrakEarth = function (scene: Scene): FakeEarth {
 	const fakeEarth = new FakeEarth(scene.fog?.color || new Color(0));
 	fakeEarth.name = "fakeearth";
 	fakeEarth.applyMatrix4(this.rootTile.matrix);
 	this.add(fakeEarth);
-	return this;
+	return fakeEarth;
 };
--- src/plugin/mapExtension/index.ts
+++ src/plugin/mapExtension/index.ts
@@ -10,46 +10,32 @@ declare module "../../map" {
 			maxLevle: number;
 			downLoading: number;
 		};
+		getTileCount(): { total: number; visible: number; leaf: number; maxLevle: number; downLoading: number };
 	}
 }
 
 Object.defineProperty(TileMap.prototype, "tileCount", {
 	get() {
-		let total = 0,
-			visible = 0,
-			maxLevel = 0,
-			leaf = 0,
-			downloading = 0;
-
-		this.rootTile.traverse((tile: Tile) => {
-			if (tile.isTile) {
-				total++;
-				tile.isLeaf && tile.inFrustum && visible++;
-				tile.isLeaf && leaf++;
-				maxLevel = Math.max(maxLevel, tile.z);
-				downloading = Tile.downloadThreads;
-			}
-		});
-		return { total, visible, leaf, maxLevle: maxLevel, downLoading: downloading };
+		return this.getTileCount();
 	},
 	configurable: true,
 });
 
-// TileMap.prototype.tileCount = function () {
-// 	let total = 0,
-// 		visible = 0,
-// 		maxLevel = 0,
-// 		leaf = 0,
-// 		downloading = 0;
+TileMap.prototype.getTileCount = function () {
+	let total = 0,
+		visible = 0,
+		maxLevel = 0,
+		leaf = 0,
+		downloading = 0;
 
-// 	this.rootTile.traverse((tile) => {
-// 		if (tile.isTile) {
-// 			total++;
-// 			tile.isLeaf && tile.inFrustum && visible++;
-// 			tile.isLeaf && leaf++;
-// 			maxLevel = Math.max(maxLevel, tile.z);
-// 			downloading = Tile.downloadThreads;
-// 		}
-// 	});
-// 	return { total, visible, leaf, maxLevle: maxLevel, downLoading: downloading };
-// };
+	this.rootTile.traverse((tile) => {
+		if (tile.isTile) {
+			total++;
+			tile.isLeaf && tile.inFrustum && visible++;
+			tile.isLeaf && leaf++;
+			maxLevel = Math.max(maxLevel, tile.z);
+			downloading = Tile.downloadThreads;
+		}
+	});
+	return { total, visible, leaf, maxLevle: maxLevel, downLoading: downloading };
+};
