e80e7b8273b1af720c7eac05fffda8cba5202104
hz_gjf@163.com <gjf123456>
2025-06-18 23:11:49 +0800

    精简wms代码

--- packages/plugin/src/mapSource/WmsSource.ts
+++ packages/plugin/src/mapSource/WmsSource.ts
@@ -1,27 +1,17 @@
 import { TileSource } from "three-tile";
 
-/**
- * WMS datasource
- */
+/**  WMS datasource */
 export class WmsSource extends TileSource {
-	public dataType: string = "image";
+	public dataType = "image";
 	public bbox = "";
-
 	public getUrl(x: number, y: number, z: number) {
-		this.bbox = tileToWebMercatorBoundingBox(x, y, z);
+		const worldSize = Math.PI * 6378137;
+		const tileSize = (2 * worldSize) / Math.pow(2, z);
+		const minX = -worldSize + x * tileSize;
+		const minY = worldSize - (y + 1) * tileSize;
+		const maxX = -worldSize + (x + 1) * tileSize;
+		const maxY = worldSize - y * tileSize;
+		this.bbox = `${minX},${minY},${maxX},${maxY}`;
 		return super.getUrl(x, y, z);
 	}
-}
-
-function tileToWebMercatorBoundingBox(x: number, y: number, z: number) {
-	const earthRadius = 6378137;
-	const worldSize = 2 * Math.PI * earthRadius;
-	const tileSize = worldSize / Math.pow(2, z);
-	const bbox = {
-		minX: -worldSize / 2 + x * tileSize,
-		minY: worldSize / 2 - (y + 1) * tileSize,
-		maxX: -worldSize / 2 + (x + 1) * tileSize,
-		maxY: worldSize / 2 - y * tileSize,
-	};
-	return `${bbox.minX},${bbox.minY},${bbox.maxX},${bbox.maxY}`;
 }
