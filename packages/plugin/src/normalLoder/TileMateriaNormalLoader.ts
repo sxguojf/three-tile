c0db3808a2d736c494b0f46310a7e4f3a08f6054
hz_gjf@163.com <gjf123456>
2025-09-11 21:09:29 +0800

    简化法向量材质加载器

--- packages/plugin/src/normalLoder/TileMateriaNormalLoader.ts
+++ packages/plugin/src/normalLoder/TileMateriaNormalLoader.ts
@@ -4,34 +4,38 @@
  *@date: 2023-04-05
  */
 
-import { ITileMaterial, ITileMaterialLoader, TileSourceLoadParamsType, version } from "three-tile";
-import { Material, MeshNormalMaterial } from "three";
+import { MeshNormalMaterial } from "three";
+import { TileMaterialLoader, version } from "three-tile";
 
 /**
  * Tile normal Material loader
  */
-export class TileMateriaNormalLoader implements ITileMaterialLoader {
+export class TileMateriaNormalLoader extends TileMaterialLoader {
 	public readonly info = {
 		version,
 		description: "Tile normal material loader.",
 	};
 	public readonly dataType: string = "normal";
 
-	public async load(params: TileSourceLoadParamsType): Promise<Material> {
-		const material = new MeshNormalMaterial({
-			transparent: params.source.transparent,
-			opacity: params.source.opacity,
-			flatShading: true,
-		});
-
-		const dispose = (evt: { target: ITileMaterial }) => {
-			evt.target.map?.dispose();
-			material.removeEventListener("dispose", dispose);
-		};
-		material.addEventListener("dispose", dispose);
-
-		material.addEventListener("dispose", dispose);
-
-		return material;
+	constructor() {
+		super();
+		this.material = new MeshNormalMaterial();
 	}
 }
+
+// export class TileMateriaNormalLoader implements ITileMaterialLoader {
+// public async load(params: TileSourceLoadParamsType): Promise<Material> {
+// 	const material = new MeshNormalMaterial({
+// 		transparent: params.source.transparent,
+// 		opacity: params.source.opacity,
+// 		flatShading: true,
+// 	});
+// 	const dispose = (evt: { target: ITileMaterial }) => {
+// 		evt.target.map?.dispose();
+// 		material.removeEventListener("dispose", dispose);
+// 	};
+// 	material.addEventListener("dispose", dispose);
+// 	material.addEventListener("dispose", dispose);
+// 	return material;
+// }
+// }
